942e44f04e271b360a990eb92a19a66462c19c78
Remi Kujawski <remi.kujawski@gmail.com>
2024-06-15 12:51:40 +0200

    Task 1 - completed

--- task-1.js
+++ task-1.js
@@ -1,4 +1,12 @@
 "use strict";
 
+const list1 = document.querySelectorAll(".item");
+
 console.log("");
 console.log("Task 1:");
+
+console.log("Number of categories: " + list1.length);
+for (let i = 0; i < list1.length; i++) {
+  console.log("Category: " + list1[i].querySelector("h2").textContent);
+  console.log("Elemens: " + list1[i].querySelectorAll("li").length);
+}
