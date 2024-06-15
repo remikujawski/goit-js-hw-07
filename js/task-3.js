76c68a6d40795596694b3f6645d4ebf0a32e7d95
Remi Kujawski <remi.kujawski@gmail.com>
2024-06-16 00:50:05 +0200

    Task 3 completed

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const nameIn = document.querySelector("#name-input");
+const nameOut = document.querySelector("#name-output");
+
+nameIn.addEventListener("input", (event) => {
+  nameOut.textContent = event.currentTarget.value;
+});
