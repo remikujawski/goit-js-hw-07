5175df155e17ea550645f89ba253abb9f03ee4f3
Remi Kujawski <remi.kujawski@gmail.com>
2024-06-17 00:00:14 +0200

    Task 4 completed

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+"use strict";
 
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (login === "" || password === "") {
+    alert("All form fields must be filled in!");
+  } else {
+    console.log({ email: login, password: password });
+    form.reset();
+  }
+}
