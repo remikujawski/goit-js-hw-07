20b2165e75a5ce53cdb9858d9270093098073a9d
Remi <remi.kujawski@gmail.com>
2024-06-19 20:22:06 +0200

    Task 6 completed

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function removeBoxes() {
+  while (boxexContainer.firstChild) {
+    boxexContainer.removeChild(boxexContainer.firstChild);
+  }
+}
+
+function createBoxes(amount) {
+  input.value = "";
+  let boxSize = 30;
+  removeBoxes();
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = box.style.height = `${boxSize}px`;
+    boxSize += 10;
+    boxexContainer.appendChild(box);
+  }
+}
+
+const boxexContainer = document.querySelector("#boxes");
+const input = document.querySelector("#controls").firstElementChild;
+const addButton = document.querySelector("button[data-create]");
+const removeButton = document.querySelector("button[data-destroy]");
+
+addButton.addEventListener("click", (event) => {
+  if (input.value > 0 && input.value <= 100) {
+    createBoxes(parseInt(input.value));
+  }
+});
+
+removeButton.addEventListener("click", (event) => removeBoxes());
